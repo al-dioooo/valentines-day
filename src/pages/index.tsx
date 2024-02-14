9a3ccb4203e652e4df142e46fb96ecc5fe34cc11
Aldio Lisafron <lisafronaldio123@gmail.com>
2024-02-14 15:07:33 +0700

    Fix react/no-unescaped-entities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                         <span className="absolute -right-12"><Star className="w-10 h-10" /></span>
                     </div>
                     <div className="relative flex font-zighead text-8xl w-fit">
-                        <span>Valentine's</span>
+                        <span>Valentine&apos;s</span>
                         <span className="absolute -left-1 -bottom-1"><Curl className="w-16 h-16" /></span>
                     </div>
                     <div className="relative p-8 -mt-20 -mr-8 text-right text-transparent font-script text-9xl bg-gradient-to-tl from-blue-300 to-pink-300 bg-clip-text">
